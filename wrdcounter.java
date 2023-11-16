1cf498432f80ff8a40884c7144f12b9a52bd8d10
ajanu33github <146116548+ajanu33github@users.noreply.github.com>
2023-11-16 19:36:03 +0530

    Add files via upload

---
+++ wrdcounter.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class wrdcounter{
+    public static void main(String[] args){
+        Scanner read=new Scanner(System.in);
+        String myString=new String();
+        System.out.print("Enter any string:");
+        myString=read.nextLine();
+        int line=0,let=0,size=0;
+
+        System.out.println("\nThe words are:");
+
+        for (int i=0;i<myString.length();i++){
+            char letter = myString.charAt(i);
+
+            boolean ch=Character.isWhitespace(letter);
+
+            if(ch||myString.length()-1==i){
+             System.out.print(letter);
+             System.out.println();
+             let++;
+             line++;
+
+            }
+            else{
+                System.out.print(letter);
+            }
+        }
+        System.out.println("\nLines:"+line);
+        System.out.println("Total no. of words:"+ let);
+        }
+    }
